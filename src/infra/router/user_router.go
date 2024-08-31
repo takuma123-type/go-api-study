cfeae20b315d8b81d76ee5299adb81dd61b7b3fa
Takuma <>
2024-08-31 20:16:34 +0900

    routerの変更

--- src/infra/router/user_router.go
+++ src/infra/router/user_router.go
@@ -27,46 +27,49 @@ func NewUserRouter(g *gin.Engine) {
 		panic("failed to connect database")
 	}
 
-	g.GET("/users", func(ctx *gin.Context) {
-		userRepoImpl := database.NewUserRepositoryImpl(db)
-		err := controller.NewUserController(presenter.NewUserPresenter(ctx), userRepoImpl).GetUserList(ctx)
-		if err != nil {
-			ctx.Error(err)
-			return
-		}
-	})
+	api := g.Group("/api")
+	{
+		api.GET("/users", func(ctx *gin.Context) {
+			userRepoImpl := database.NewUserRepositoryImpl(db)
+			err := controller.NewUserController(presenter.NewUserPresenter(ctx), userRepoImpl).GetUserList(ctx)
+			if err != nil {
+				ctx.Error(err)
+				return
+			}
+		})
 
-	g.GET("/users/:id", func(ctx *gin.Context) {
-		type reqStruct struct {
-			ID string `uri:"id"`
-		}
-		var req reqStruct
-		if err := ctx.ShouldBindUri(&req); err != nil {
-			ctx.JSON(400, gin.H{"status": "bad request"})
-			return
-		}
+		api.GET("/users/:id", func(ctx *gin.Context) {
+			type reqStruct struct {
+				ID string `uri:"id"`
+			}
+			var req reqStruct
+			if err := ctx.ShouldBindUri(&req); err != nil {
+				ctx.JSON(400, gin.H{"status": "bad request"})
+				return
+			}
 
-		in := userinput.GetUserByIDInput{ID: req.ID}
-		userRepoImpl := database.NewUserRepositoryImpl(db)
-		err := controller.NewUserController(presenter.NewUserPresenter(ctx), userRepoImpl).GetUserByID(ctx, &in)
-		if err != nil {
-			ctx.Error(err)
-			return
-		}
-	})
+			in := userinput.GetUserByIDInput{ID: req.ID}
+			userRepoImpl := database.NewUserRepositoryImpl(db)
+			err := controller.NewUserController(presenter.NewUserPresenter(ctx), userRepoImpl).GetUserByID(ctx, &in)
+			if err != nil {
+				ctx.Error(err)
+				return
+			}
+		})
 
-	g.POST("/users", func(ctx *gin.Context) {
-		var in userinput.CreateUserInput
-		if err := ctx.ShouldBindJSON(&in); err != nil {
-			ctx.JSON(400, gin.H{"status": "bad request"})
-			return
-		}
+		api.POST("/users", func(ctx *gin.Context) {
+			var in userinput.CreateUserInput
+			if err := ctx.ShouldBindJSON(&in); err != nil {
+				ctx.JSON(400, gin.H{"status": "bad request"})
+				return
+			}
 
-		userRepoImpl := database.NewUserRepositoryImpl(db)
-		err := controller.NewUserController(presenter.NewUserPresenter(ctx), userRepoImpl).CreateUser(ctx, &in)
-		if err != nil {
-			ctx.Error(err)
-			return
-		}
-	})
+			userRepoImpl := database.NewUserRepositoryImpl(db)
+			err := controller.NewUserController(presenter.NewUserPresenter(ctx), userRepoImpl).CreateUser(ctx, &in)
+			if err != nil {
+				ctx.Error(err)
+				return
+			}
+		})
+	}
 }
