55b0a7a63cd5c5751b30cd4ac34acc9a000b61fe
Takuma <>
2024-10-14 13:42:11 +0900

    新規作成ならidは引数で受け取らずに、関数の中で生成

--- src/domain/contractrequestdm/contract_request_factory.go
+++ src/domain/contractrequestdm/contract_request_factory.go
@@ -1,11 +1,12 @@
 package contractrequestdm
 
 import (
+	"github.com/google/uuid"
 	"github.com/takuma123-type/go-api-study/src/domain/plandm"
 )
 
-func GenContractRequestIfCreate(id, planID, message string) (*ContractRequest, error) {
-	contractRequestID := ContractRequestID(id)
+func GenContractRequestIfCreate(planID, message string) (*ContractRequest, error) {
+	contractRequestID := ContractRequestID(uuid.New().String())
 	return newContractRequest(
 		contractRequestID,
 		plandm.PlanID(planID),
