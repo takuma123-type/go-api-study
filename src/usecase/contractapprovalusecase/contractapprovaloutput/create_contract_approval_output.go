c74ce96de1eb1f0ec9902a7e2f4f9bdc00e5df5f
Takuma <>
2024-10-14 20:47:31 +0900

    ユースケースの作成

---
+++ src/usecase/contractapprovalusecase/contractapprovaloutput/create_contract_approval_output.go
@@ -0,0 +1,7 @@
+package contractapprovaloutput
+
+type CreateContractApprovalOutput struct {
+	ID      string `json:"id,omitempty"`
+	PlanID  string `json:"plan_id,omitempty"`
+	Message string `json:"message,omitempty"`
+}
